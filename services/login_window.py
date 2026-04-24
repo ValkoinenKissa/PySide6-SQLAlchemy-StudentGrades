7aa4dc7ff8a0be9291fcd671bc09e9a49c257dcd
Alberto Bollo <albertobollo7@protonmail.com>
2026-04-24 13:48:14 +0200

    Added comments

--- services/login_window.py
+++ services/login_window.py
@@ -29,20 +29,24 @@ class LoginWindow(QWidget):
         self.setFixedSize(self.size())
         self.setWindowTitle("Login")
 
-        self.next_window: Optional[QWidget] = None  # evita warning
+        self.next_window: Optional[QWidget] = None
+
+
+        # Configuramos las acciones de los botones
 
         self.comboRole.addItems([UserType.PROFESOR.value, UserType.ESTUDIANTE.value])
-
         self.btnLogin.clicked.connect(self.handle_login)
         self.btnClear.clicked.connect(self.handle_clear)
         self.btnExit.clicked.connect(self.close)
         self.editPassword.returnPressed.connect(self.handle_login)
 
+    # Función para gestionar el login
     def handle_login(self):
         username = self.editUsername.text().strip()
         password = self.editPassword.text().strip()
         role = self.comboRole.currentText()
 
+        # Verificación campos usuario y pass
         if not username:
             QMessageBox.warning(self, "Error", "El campo usuario no puede estar vacío")
             return
@@ -50,24 +54,30 @@ class LoginWindow(QWidget):
             QMessageBox.warning(self, "Error", "El campo contraseña no puede estar vacío")
             return
 
+        # Se opbtiene conexion con el conector de la bd y se le pasa la conexion al DAO de usuarios
         try:
             with get_session() as session:
                 user_dao = UserDAOImp(session)
+                #Se ejecuta la validación del usuario
                 user = user_dao.validate_login(username, password)
 
                 if user is None:
+                    # Si es nulo se muestra el mensaje de alerta
                     QMessageBox.warning(self, "Error", "Usuario o contraseña incorrectos")
                     return
 
+                #Si el usuario es correcto, pero el rol seleccionado no es el rol correcto
                 if user.user_type is None or user.user_type.value != role:
                     QMessageBox.critical(self, "Error", "El rol seleccionado no coincide con tu usuario")
                     return
 
+                #Si el usuario tiene el rol profesor, se carga la ventana del profesor, sino la del alumno
                 if user.user_type == UserType.PROFESOR:
                     self.next_window = TeacherWindow(user_id=user.id)
                 else:
                     self.next_window = StudentWindow(user_id=user.id)
 
+            # Se muestra la siguiente ventana seleccionada y se oculta la actual
             if self.next_window is not None:
                 self.next_window.show()
                 self.close()
@@ -75,6 +85,7 @@ class LoginWindow(QWidget):
         except Exception as e:
             QMessageBox.critical(self, "Error", f"Error al iniciar sesión: {e}")
 
+    # funcion para limpiar los campos y el combobox
     def handle_clear(self):
         self.editUsername.clear()
         self.editPassword.clear()
